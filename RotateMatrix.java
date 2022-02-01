5e3586482073fac2f111a1e35a86e42048083fbf
Kartik Kumar <mysticfreak1499@gmail.com>
2022-02-01 22:53:10 +0530

    February 1, 2022 10:53 PM

--- RotateMatrix.java
+++ RotateMatrix.java
@@ -2,24 +2,28 @@ import java.util.*;
 import java.io.*;
 
 public class RotateMatrix {
-    static int[][] rotateMatrix(int[][] arr){
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr.length; j++){
-                
+    static int[][] rotateMatrix(int[][] a){
+        int[][] b = new int[a.length][a[0].length];
+
+        for(int i=0; i<a.length; i++){
+            for(int j=0; j<a.length; j++){
+                b[j][a.length-i]=a[i][j];
             }
         }
+
+        return b;
     }
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = br.read();
-        int arr[][] = new int[n][n];
+        int a[][] = new int[n][n];
 
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                arr[i][j] = br.read();
+                a[i][j] = br.read();
             }
         }
-        System.out.print(rotateMatrix(arr));
+        System.out.print(rotateMatrix(a));
         }
 }
